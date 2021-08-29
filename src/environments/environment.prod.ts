93e9c2d001a48bd6166e293ed641790f4eda90a6
HermanDeadpool98 <89682260+HermanDeadpool98@users.noreply.github.com>
2021-08-29 16:44:35 +0200

    Add files via upload

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+  gateway: '',
+  callback: 'http://localhost:4200/callback',
+  domain: 'dev-481aj0ux.us.auth0.com',
+  clientId: 'fYU2MlScel2JOnhyAyAau3JSpFm3Db04',
+  audience: 'https://example.com/todobackend'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  gateway: 'http://localhost:3000',
+  callback: 'http://localhost:4200/callback',
+  domain: 'dev-481aj0ux.us.auth0.com',
+  clientId: 'fYU2MlScel2JOnhyAyAau3JSpFm3Db04',
+  audience: 'https://example.com/todobackend'
+};
